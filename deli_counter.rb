dfba76b9f383dddb2e10eebf5ea8838923936fda
Sid He <xhe518@gmail.com>
2018-06-22 00:25:02 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,17 @@
 def line(katz_deli)
-  def names(katz_deli)
-    katz_deli.each_with_index.map do |customer, index|
-      "#{index+1}. #{customer}"
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  elsif katz_deli.length > 1
+    def names(katz_deli)
+      katz_deli.each_with_index.map do |customer, index|
+        "#{index+1}. #{customer}"
+      end
     end
+    #names(katz_deli) returns ["1. Ari", "2. Ben"]
+    joined_names = names(katz_deli).join(" ")
+    puts "The line is currently: #{joined_names}"
+  else
+    false
   end
-  #names(katz_deli) returns ["1. Ari", "2. Ben"]
-  joined_names = names(katz_deli).join(" ")
-  puts "The line is currently: #{joined_names}"
 end
 #def take_a_number(katz_deli, person)
