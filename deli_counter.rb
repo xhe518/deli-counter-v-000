15a8be36aa8df57ca24f238ec4f8657611d0f1c1
Sid He <xhe518@gmail.com>
2018-06-22 00:20:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def line(katz_deli)
+  def names(katz_deli)
+    katz_deli.each_with_index.map do |customer, index|
+      "#{index+1}. #{customer}"
+    end
+  end
+  #names(katz_deli) returns ["1. Ari", "2. Ben"]
+  names(katz_deli).join(" ")
+  #
+
+  #  The line is currently:
+end
+#def take_a_number(katz_deli, person)
