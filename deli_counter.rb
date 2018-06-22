2a4ed6cee48d02e47a6a5fc5adcdcd3645eb191c
Sid He <xhe518@gmail.com>
2018-06-22 00:42:50 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,4 @@
+#----------------line------------------
 def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
@@ -13,4 +14,11 @@ def line(katz_deli)
   else
     false
   end
+end
+
+#-----------------take_a_number-------------------
+def take_a_number(katz_deli, customer_name)
+#  katz_deli.each_with_index do |customer, index|
+  line_number = katz_deli.index(customer_name) + 1  
+  puts "Welcome, #{customer_name}. You are number #{line_number} in line."
 end
